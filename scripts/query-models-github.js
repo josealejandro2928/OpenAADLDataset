29c84019ed32f1f5bb53e4150794d8708188f421
Jose Alejandro Concepcion Alvarez <jalejandroc2928@gmail.com>
2023-05-11 21:15:05 +0200

    feat: Adding filters to the repo

--- scripts/query-models-github.js
+++ scripts/query-models-github.js
@@ -1,6 +1,7 @@
 const API_GITHUB_URI = "https://api.github.com/search/repositories"
 const fs = require("node:fs");
 const path = require("node:path");
+const avoidedRepos = ["https://github.com/josealejandro2928/OpenAADLDataset"];
 
 async function queryRepositories(queryParams = {}) {
     const searchParams = new URLSearchParams({ ...queryParams, sort: "stars", order: "desc" });
@@ -24,7 +25,7 @@ async function main() {
         for (const data of res) {
             data.items.map((item) => setUris.add(item.html_url))
         }
-        const data = [...setUris].map((item)=>`"${item}",`).join("\n");
+        const data = [...setUris].filter(x => avoidedRepos.indexOf(x) == -1).map((item) => `"${item}",`).join("\n");
         fs.writeFileSync(path.join(__dirname, "result.txt"), data);
     } catch (e) {
         console.error("ERROR: ", e);
